259d7694617ce26e2a6a282f62a8f149342190fb
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2023-10-25 14:17:17 +0530

    project _structure

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages,setup
 from typing import List
 
+
+hyphen_e_dot='-e'
 def get_requirement(file_path:str)->List[str]:
 
     requirements=[]
@@ -8,6 +10,9 @@ def get_requirement(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace('\n','') for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
 
     
     return requirements
