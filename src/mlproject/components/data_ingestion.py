6f31d2a514109aebffd76d3652a0c6c7edda9a80
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2023-10-26 16:14:45 +0530

    udated ingestion

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -31,9 +31,9 @@ class Dataingestion:
 
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=43)
 
-            df.to_csv(self.ingestion_config.train_data_Path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_Path,index=False,header=True)
 
-            df.to_csv(self.ingestion_config.test_data_Path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_Path,index=False,header=True)
 
             logging.info("data ingestion completed")
 
