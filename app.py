ef5c784aaace63be50960c325dadd48f157051f6
Swapnil Nandalalsing Pardeshi <126460594+swapnil94pardeshi@users.noreply.github.com>
2023-11-02 12:41:53 +0530

    model trainer

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.mlproject.logger import logging
 from src.mlproject.components.data_ingestion import Dataingestion
 from src.mlproject.components.data_ingestion import Dataingestionconfig
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 import sys
 from src.mlproject.utils import read_sql_data,save_object
@@ -19,7 +20,12 @@ if __name__=="__main__":
 
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_Path,test_data_Path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_Path,test_data_Path)
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
+
 
     except Exception as e:
         logging.info("Custom Exception")
